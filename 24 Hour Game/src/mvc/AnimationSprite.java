26c003326fed6beb32158d4128f55339b3922728
Dylan Swiggett <omnipotentperson@gmail.com>
2012-02-16 17:40:41 -0800

    Added animated sprites, and a small test for them.

---
+++ 24 Hour Game/src/mvc/AnimationSprite.java
@@ -0,0 +1,115 @@
+package mvc;
+
+import java.awt.Rectangle;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
+public class AnimationSprite extends Sprite{
+	
+	private float x, y, w, h, depth, r;
+	
+	private ArrayList<Texture> frames;
+	private Queue<String> texturePathBuffer;
+	
+	private int frameCount;	// Counts the number of elapsed frames.
+	private int frameDelay;	// Time between frames, in # of times drawn.
+	
+	
+	public AnimationSprite(float x, float y, float w, float h, float depth, int speed, String texturePath){
+		frames = new ArrayList<Texture>();
+		texturePathBuffer = new LinkedList<String>();
+		
+		texturePathBuffer.add(texturePath);
+		
+		frameCount = 0;
+		frameDelay = speed;
+		
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.depth = depth;
+	}
+	
+	public void addFrame(String texturePath){
+		texturePathBuffer.add(texturePath);
+	}
+
+	@Override
+	public Rectangle getBoundingBox() {
+		return new Rectangle((int) x, (int) y, (int) w, (int) h);
+	}
+
+	@Override
+	public void draw() {
+		while(texturePathBuffer.size() > 0){
+			try {
+				Texture texture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(texturePathBuffer.poll()));
+				GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+				GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+				frames.add(texture);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		Texture currentTexture = frames.get((int) (frameCount / frameDelay));
+		
+		/*
+		 * Rotate the object around the z axis, then translate it to its appropriate position in space,
+		 * x or y + w or h / 2, so that the image rotates around its center.
+		 */
+		GL11.glPushMatrix();
+		GL11.glTranslatef(x + w / 2, y + h / 2, 0);
+		GL11.glRotatef(r, 0, 0, 1);
+		
+		float xDist = w / 2;
+		float yDist = h / 2;
+		
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		
+		GL11.glColor3f(1, 1, 1);
+		
+		currentTexture.bind();
+		
+		GL11.glBegin(GL11.GL_TRIANGLE_FAN);
+		GL11.glTexCoord2f(0, 1);
+		GL11.glVertex3f(-xDist, -yDist, depth);
+		GL11.glTexCoord2f(1, 1);
+		GL11.glVertex3f(xDist, -yDist, depth);
+		GL11.glTexCoord2f(1, 0);
+		GL11.glVertex3f(xDist, yDist, depth);
+		GL11.glTexCoord2f(0, 0);
+		GL11.glVertex3f(-xDist, yDist, depth);
+		GL11.glEnd();
+		
+		/*
+		 * Draw outline
+		 */
+		
+		GL11.glColor3f(0, 0, 0);
+		
+		GL11.glBegin(GL11.GL_LINE_LOOP);
+		GL11.glVertex3f(-xDist, -yDist, depth);
+		GL11.glVertex3f(xDist, -yDist, depth);
+		GL11.glVertex3f(xDist, yDist, depth);
+		GL11.glVertex3f(-xDist, yDist, depth);
+		GL11.glEnd();
+		
+		GL11.glPopMatrix();
+		
+		frameCount += 1;
+		if (frameCount >= frameDelay * frames.size()){
+			frameCount = 0;
+		}
+	}
+
+}
--- 24 Hour Game/src/mvc/Model.java
+++ 24 Hour Game/src/mvc/Model.java
@@ -59,10 +59,21 @@ public class Model extends Thread {
 		
 		sprites.add(new TextureSprite(100, 100, 100, 100, 300, "/data/test.png"));
 		
+//		AnimationSprite animSprite = new AnimationSprite(400, 400, 200, 200, 200, "/data/test.png");
+//		
+//		sprites.add(animSprite);
+		
 		sprites.add(new RectSprite(50, 50, 50, 50, 50, Color.red));
 		sprites.add(new ExtrudeSprite(100, 100, 100, 100, 299, Color.red));
 		
 		sprites.add(new TextureSprite(-1600, -800, 3200, 1600, 0, "/data/lavabg.png"));
+		
+		AnimationSprite animSprite = new AnimationSprite(400, 400, 200, 200, 200, 10, "/data/testFrame1.png");
+		animSprite.addFrame("/data/testFrame2.png");
+		animSprite.addFrame("/data/testFrame3.png");
+		animSprite.addFrame("/data/testFrame2.png");
+		
+		sprites.add(animSprite);
 		
 		Light light = new Light();
 		
