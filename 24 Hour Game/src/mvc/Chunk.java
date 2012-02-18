ac48605fcef0889f447ee7f15f48b88bc55bae1a
Dylan Swiggett <omnipotentperson@gmail.com>
2012-02-18 14:29:39 -0800

    Finished model (for now), some updates to chunk, et. al.

--- 24 Hour Game/src/mvc/Chunk.java
+++ 24 Hour Game/src/mvc/Chunk.java
@@ -8,23 +8,29 @@ public class Chunk extends Sprite{
 	public static final int CHUNKDIMENSION = 32; // The number of walls/pixels on each side of the chunk
 	public static final int WALLDIMENSION = 10;	 // The number of pixels on each side of a wall.
 	
+	int[][] tiles;
+	
+	/*
+	 * Definitions of tile values
+	 */
+	public static final int SPACE = 0;
+	public static final int WALL = 1;
+	
 	int x, y;
 	
 	private HashMap<String, String> properties;
 	
 	public Chunk(){
-		
+		tiles = new int[CHUNKDIMENSION][CHUNKDIMENSION];
 	}
 	
 	@Override
 	public Rectangle getBoundingBox() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Rectangle(x, y, CHUNKDIMENSION * WALLDIMENSION, CHUNKDIMENSION * WALLDIMENSION);
 	}
 
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
 		
 	}
 
--- 24 Hour Game/src/mvc/Wall.java
+++ 24 Hour Game/src/mvc/Wall.java
@@ -5,7 +5,7 @@ import org.newdawn.slick.Color;
 public class Wall extends ExtrudeSprite{
 
 	public Wall(float x, float y, float w, float h) {
-		super(x, y, w, h, View.INITDISTANCE, Color.black);
+		super(x, y, w, h, View.INITDISTANCE + 2000, Color.black);
 		// TODO Auto-generated constructor stub
 	}
 
