a58e14967f829aedf6bccbef9a41156f57338382
Dylan Swiggett <omnipotentperson@gmail.com>
2012-02-19 07:28:28 -0800

    Spectre

---
+++ 24 Hour Game/src/mvc/Spectre.java
@@ -0,0 +1,66 @@
+package mvc;
+
+import java.util.HashMap;
+
+public class Spectre extends MultiSprite{
+
+	HashMap<String, Sprite> sprites;
+//	private Sprite curSprite;	//Current sprite;
+	
+	Vector3D spectreVelocity, spectrePosition, spectreLastPosition;
+	float spectreSensitivity;
+	float rot;
+	
+	float health = 50;
+	float strength = .1f;
+	
+	public Spectre(Sprite initialSprite, String spriteName) {
+		super(initialSprite, spriteName);
+		
+		sprites = new HashMap<String, Sprite>();
+		sprites.put(spriteName, initialSprite);
+		
+		curSprite = initialSprite;
+		curSpriteName = spriteName;
+		
+		spectreSensitivity = 0.9f;
+		spectreVelocity = new Vector3D(0, 0, 0);
+		spectrePosition = initialSprite.p;
+		spectreLastPosition = initialSprite.p;
+		rot = 0;
+	}
+	
+	@Override
+	public void draw() {
+//		if (curSpriteName.equals("walking")){
+//			int f = ((AnimationSprite) curSprite).getFrameCount();
+//			int delay = ((AnimationSprite) curSprite).frameDelay;
+////			System.out.println(f);
+//			if (f == 0 * delay || f == 9 * delay){
+//				System.out.println("Step");
+//			}
+//		}
+		curSprite.setRot(rot);
+		curSprite.draw();
+	}
+	
+	public void setRot(float rot){
+		this.rot = rot;
+	}
+	
+	public float getWidth() {
+		return (float) curSprite.getBoundingBox().getWidth();
+	}
+	
+	public float getHeight() {
+		return (float) curSprite.getBoundingBox().getHeight();
+	}
+	
+	public void hurt(float damage){
+		health -= damage;
+	}
+	
+	public float getDamage(){
+		return strength;
+	}
+}
